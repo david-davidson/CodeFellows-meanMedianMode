5bb614da9b7c144ad3a51cd4649ec732ffa5eb86
David Davidson <daviddavidson.1988@gmail.com>
2014-08-05 09:25:33 -0700

    Initial commit of mean/median/mode script

---
+++ numbers.js
@@ -0,0 +1,58 @@
+// Since median sometimes requires mean, let's put all three in functions
+var mean = function(numbers) {
+	var meanSum = 0;
+	for (var i = 0; i < numbers.length; i++) {
+		meanSum += new Number(numbers[i]);
+	}
+	return (meanSum / numbers.length);
+}
+var median = function(numbers) {
+	var numbers = numbers.slice().sort(function(a, b) { 
+		return a - b;  // .slice() lets us sort a copy, not the original: cleaner
+	});
+	if (numbers.length % 2 != 0) {
+		// Odd number of arguments? Return the middle number...
+		var middle = (numbers.length - 1) / 2;
+		return numbers[middle]; 
+	} else {
+		// ...Even? Return the mean of the middle two
+		var lowerMiddle = Math.floor((numbers.length - 1) / 2);
+		var higherMiddle = Math.ceil((numbers.length - 1) / 2);
+		return mean([numbers[lowerMiddle], numbers[higherMiddle]]);
+	}
+}
+var mode = function(numbers) {
+	// H/T http://stackoverflow.com/questions/2440295/extracting-the-most-duplicate-value-from-an-array-in-javascript-with-jquery#answer-2440343
+	var numAppearances = {}; // Use an object as an associative array
+	var prevMax = 0;
+	var winners = [];
+	for (var i = 0; i < numbers.length; i++) {
+		// Increment count if it's available; otherwise, start a new one
+		numAppearances[numbers[i]] = (numAppearances[numbers[i]] || 0) + 1;
+		if (numAppearances[numbers[i]] > prevMax) {
+			prevMax = numAppearances[numbers[i]];
+			winners = []; // Empty previous winner(s)
+			winners.push(numbers[i]);
+		} else if (numAppearances[numbers[i]] == prevMax) {
+			winners.push(numbers[i]); // DON'T empty previous winner(s)
+		}
+	}
+	if (winners.length > 1) {
+		return "It's a tie between " + winners.join(' and ');
+	} else {
+		return winners[0];
+	}
+}
+if (process.argv.length <= 2) {
+	console.log('Uh oh, you didn\'t give me any numbers!');
+} else {
+	process.argv.splice(0,2); // Trim argv[] to just the "real" arguments
+	for (var i = 0; i < process.argv.length; i++) {
+		if (isNaN(process.argv[i])) {
+			process.argv.splice(i, 1); // Remove non-numbers, just to be safe
+		}
+	}
+	console.log('mean: ' + mean(process.argv));
+	console.log('median: ' + median(process.argv));
+	console.log('mode: ' + mode(process.argv));
+}
